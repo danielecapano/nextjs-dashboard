9238c0e5fbd93e73b59de4a849367b34877cd39b
danielecapano <daniele.capano@gmail.com>
2024-06-05 17:45:02 +0200

    Add pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Outfit} from 'next/font/google';
+export const outfit = Outfit({subsets: ['latin']});
+
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
